1d16d90620280ea799fac1f99d2290d36e5d5d0d
yukihirop <te108186@gmail.com>
2018-07-16 19:23:11 +0900

    Delete unused method in Utility

--- lib/fcoin/endpoint/utility.rb
+++ lib/fcoin/endpoint/utility.rb
@@ -3,17 +3,6 @@ module Fcoin
   module Endpoint
     module Utility
       private
-      
-      def required_missing_error(required, *required_keys)
-        missing_keys = []
-        required_keys.each do |key|
-           missing_keys << key.to_sym if required[key].nil?
-        end
-        unless missing_keys.blank?
-          error_message = "Required arguments: #{missing_keys.join(', ')} is missing"
-          raise ArgumentError.new(error_message)
-        end
-      end
 
       def sort_payload(data)
         # sort by alphabel ASC
