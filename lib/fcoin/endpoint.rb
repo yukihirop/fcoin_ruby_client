6bd03a55a3a20422895fc3dca6e625bff5467fd9
yukihirop <te108186@gmail.com>
2018-07-16 19:11:52 +0900

    Implement Fcoin::Endpoint::Orders

--- lib/fcoin/endpoint.rb
+++ lib/fcoin/endpoint.rb
@@ -1,11 +1,13 @@
 require_relative 'endpoint/public'
 require_relative 'endpoint/market'
 require_relative 'endpoint/accounts'
+require_relative 'endpoint/orders'
 
 module Fcoin
   module Endpoint
     include Public
     include Market
     include Accounts
+    include Orders
   end
 end
