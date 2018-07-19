675ca3bf74af308674767b5cfa16048b3d94bc04
yukihirop <te108186@gmail.com>
2018-07-20 00:19:12 +0900

    Replace from require to requre_relative

--- lib/fcoin.rb
+++ lib/fcoin.rb
@@ -1,6 +1,7 @@
-require "fcoin/version"
-require "fcoin/configuration"
-require "fcoin/client"
+require_relative "fcoin/version"
+require_relative "fcoin/configuration"
+require_relative "fcoin/client"
+require_relative "fcoin/realtime/client"
 
 module Fcoin
   extend Configuration
