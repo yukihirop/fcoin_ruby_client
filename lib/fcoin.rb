6853ddb62c7bc15fb503136c70dbad16a6cd5d61
yukihirop <te108186@gmail.com>
2018-07-22 15:01:47 +0900

    Configure config

--- lib/fcoin.rb
+++ lib/fcoin.rb
@@ -2,7 +2,12 @@ require_relative "fcoin/version"
 require_relative "fcoin/configuration"
 require_relative "fcoin/client"
 require_relative "fcoin/realtime/client"
+require 'config'
 
 module Fcoin
   extend Configuration
+
+  Config.load_and_set_settings([
+    File.expand_path('../fcoin/config/settings.yml',__FILE__)
+  ])
 end
