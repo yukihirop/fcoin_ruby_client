73ad866f14aaaa2c2e713328e324a0bac62da4c9
yukihirop <te108186@gmail.com>
2018-07-29 13:22:36 +0900

    Add docment for Fcoin::Endpoint

--- lib/fcoin/endpoint/market.rb
+++ lib/fcoin/endpoint/market.rb
@@ -1,17 +1,42 @@
 require_relative 'utility'
 require_relative '../validator'
 
-# @see https://developer.fcoin.com/jp.html?javascript#32c808cbe5
 # Scope Fcoin::API
 module Fcoin
   module Endpoint
     module Market
-      # GET https://api.fcoin.com/v2/market/ticker/$symbol
+      # Returns ticker info of symbol.
+      #
+      # curl: GET https://api.fcoin.com/v2/market/ticker/$symbol
+      #
+      # @example get ethusdt ticker info
+      #  client = Fcoin::Client.new
+      #  puts client.market_ticker(symbol: :ethusdt) #=> {"status"=>0, "data"=>{"ticker"=>{"latest_price"=>469.43, "most_recent_trade_vol"=>0.002, "max_buy_price"=>469.43, "max_buy_amount"=>7.6318, "min_sell_price"=>469.44, "min_sell_amount"=>0.0011, "trade_price_yesterday"=>468.75, "highest_price_today"=>472.0, "lowest_price_today"=>461.76, "btcusdt_base_vol_today"=>44442.419672207, "btcusdt_base_price_today"=>20784202.592831347}, "type"=>"ticker.ethusdt", "seq"=>100521295}}
+      #
+      #
+      # @see https://developer.fcoin.com/zh.html#ticker
+      # @raise [ArgumentError] If the symbol does not have.
+      # @param symbol [String or Symbol] a text string with the name of the current financial instrument.
+      # @return [Hash] Returns ticker info.
       def market_ticker(symbol:)
         get("market/ticker/#{symbol}", false)
       end
 
-      # GET https://api.fcoin.com/v2/market/depth/$level/$symbol
+      # Returns depth info of symbol specified by level.
+      #
+      # curl: GET https://api.fcoin.com/v2/market/depth/$level/$symbol
+      #
+      # @example get ethusdt depth info specified by L20 level.
+      #  client = Fcoin::Client.new
+      #  puts client.market_ticker(symbol: :ethusdt, level: :L20) #=> {"status"=>0, "data"=>{"bids"=>[{"price"=>468.16, "amount"=>0.009}, {"price"=>468.15, "amount"=>0.0077}, {"price"=>468.14, "amount"=>0.02}, {"price"=>468.1, "amount"=>0.3723}, {"price"=>468.09, "amount"=>0.1268}, {"price"=>468.07, "amount"=>0.02}, {"price"=>468.03, "amount"=>0.0025}, {"price"=>468.02, "amount"=>0.01}, {"price"=>468.0, "amount"=>0.0896}, {"price"=>467.97, "amount"=>0.9827}, {"price"=>467.83, "amount"=>0.01}, {"price"=>467.81, "amount"=>0.01}, {"price"=>467.8, "amount"=>0.04}, {"price"=>467.78, "amount"=>0.0146}, {"price"=>467.77, "amount"=>0.0016}, {"price"=>467.76, "amount"=>0.01}, {"price"=>467.75, "amount"=>0.01}, {"price"=>467.74, "amount"=>0.01}, {"price"=>467.73, "amount"=>0.4237}, {"price"=>467.72, "amount"=>0.01}], "asks"=>[{"price"=>468.22, "amount"=>0.7}, {"price"=>468.26, "amount"=>0.7}, {"price"=>468.28, "amount"=>0.5952}, {"price"=>468.56, "amount"=>0.0027}, {"price"=>468.57, "amount"=>0.9921}, {"price"=>468.58, "amount"=>0.06}, {"price"=>468.59, "amount"=>0.3599}, {"price"=>468.6, "amount"=>30.57797087}, {"price"=>468.61, "amount"=>0.01}, {"price"=>468.62, "amount"=>0.003}, {"price"=>468.63, "amount"=>0.005}, {"price"=>468.64, "amount"=>0.03}, {"price"=>468.67, "amount"=>0.0031}, {"price"=>468.68, "amount"=>0.3966}, {"price"=>468.7, "amount"=>0.05}, {"price"=>468.71, "amount"=>0.1}, {"price"=>468.74, "amount"=>0.01}, {"price"=>468.75, "amount"=>0.0021}, {"price"=>468.76, "amount"=>0.0046}, {"price"=>468.77, "amount"=>0.0023}], "ts"=>1532831776008, "seq"=>100535850, "type"=>"depth.L20.ethusdt"}}
+      #
+      #
+      # @see https://developer.fcoin.com/zh.html#50f4407ea4
+      # @raise [ArgumentError] If the symbol or level does not have.
+      # @raise [InvalidValueError] If symbol or level is invalid.
+      # @param symbol [String or Symbol] a text string with the name of the current financial instrument.
+      # @param level [String or Symbol] Level of depth chart. level must be included in [L20, L40, full].
+      # @return [Hash] Returns depth info.
       def market_depth(symbol:, level:)
         validator = Fcoin::Validator.build(level: level, method_name: __method__)
         if skip_validation || validator.valid?
@@ -21,12 +46,38 @@ module Fcoin
         end
       end
 
-      # GET https://api.fcoin.com/v2/market/trades/$symbol
+      # Returns trades info of symbol.
+      #
+      # curl: GET https://api.fcoin.com/v2/market/trades/$symbol
+      #
+      # @example get ethusdt trades info.
+      #  client = Fcoin::Client.new
+      #  puts client.market_trades(symbol: :ethusdt) #=> {"status"=>0, "data"=>[{"amount"=>0.1811, "ts"=>1532832312857, "id"=>100541250000, "side"=>"buy", "price"=>467.95}]}
+      #
+      #
+      # @see https://developer.fcoin.com/zh.html#6477a1394e
+      # @raise [ArgumentError] If the symbol does not have.
+      # @param symbol [String] a text string with the name of the current financial instrument.
+      # @return [Hash] Returns trades info.
       def market_trades(symbol:)
         get("market/trades/#{symbol}", false)
       end
 
-      # GET https://api.fcoin.com/v2/market/candles/$resolution/$symbol
+      # Returns candles info of symbol specified by resolution.
+      #
+      # curl: GET https://api.fcoin.com/v2/market/candles/$resolution/$symbol
+      #
+      # @example get monthly(MN) ethusdt candles info.
+      #  client = Fcoin::Client.new
+      #  puts client.market_candles(symbol: :ethusdt, resolution: :D1) #=> {"status"=>0, "data"=>[{"open"=>449.1, "close"=>467.67, "high"=>516.09, "quote_vol"=>1403153842.5110748, "id"=>1530374400, "count"=>6058571, "low"=>418.29, "seq"=>10055429000001, "base_vol"=>3042620.012726224}, {"id"=>1527782400, "seq"=>7385778300000, "high"=>555.13, "low"=>406.11, "open"=>523.97, "close"=>449.04, "count"=>20382481, "base_vol"=>28258854.226292703, "quote_vol"=>13579650310.735054}, {"id"=>1525104000, "seq"=>2847062700000, "high"=>582.99, "low"=>559.66, "open"=>563.23, "close"=>575.77, "count"=>4617, "base_vol"=>4159.317883344, "quote_vol"=>2380070.431548223}]}
+      #
+      #
+      # @see https://developer.fcoin.com/zh.html#candle
+      # @raise [ArgumentError] If the symbol or resolution does not have.
+      # @raise [InvalidValueError] If symbol or resolution is invalid.
+      # @param symbol [String or Symbol] a text string with the name of the current financial instrument.
+      # @param resolution [String or Symbol] period of candles chart. resolution must be included in [M1, M3, M5, M15, M30, H1, H4, H6, D1, W1, MN].
+      # @return [Hash] Returns candles info.
       def market_candles(symbol:, resolution:)
         validator = Fcoin::Validator.build(resolution: resolution, method_name: __method__)
         if skip_validation || validator.valid?
--- lib/fcoin/endpoint/orders.rb
+++ lib/fcoin/endpoint/orders.rb
@@ -1,20 +1,31 @@
 require_relative 'utility'
 require_relative '../validator'
 
-# @see https://developer.fcoin.com/jp.html?javascript#32c808cbe5
 # Scope Fcoin::API
 module Fcoin
   module Endpoint
     module Orders
       include Utility
-
-      # POST https://api.fcoin.com/v2/orders
-      # payload:
-      # - symbol:      e.x) ethusdt
-      # - side:             sell  or buy
-      # - type:             limit or market
-      # - price:
-      # - amount:
+      # Create order.
+      #
+      # curl: POST https://api.fcoin.com/v2/orders
+      #
+      # @note This method can not be invoked without authentication.
+      #
+      # @example Create an order to sell 0.010eth at a price of 1000usdt.
+      #  client = Fcoin::Client.new(api_key: ENV['FCOIN_API_KEY'], secret_key: ENV['FCOIN_SECRET_KEY'])
+      #  puts client.create_order(symbol: :ethusdt, side: :sell, type: :limit, price: 1000, amount: 0.001) #=> {"status":0,"data":"R0moy92q4Qaf_G*********wwJM2bz_Zyacp-Ek8="}'
+      #
+      #
+      # @see https://developer.fcoin.com/zh.html#4a3e521c3b
+      # @raise [ArgumentError] If the symbol or side or type or price or amount does not have.
+      # @raise [InvalidValueError] If symbol or side or type or price or amount is invalid.
+      # @param symbol [String or Symbol] Transaction of pair
+      # @param side [String or Symbol] Direction of the transaction
+      # @param type [String or Symbol] Order type.
+      # @param price [Float]
+      # @param amount [Float]
+      # @return [Hash] Returns receipt contains order_id.
       def create_order(symbol:, side:, type:, price:, amount:)
         payload = { symbol: symbol, side: side, type: type, price: price, amount: amount }
         validator = Fcoin::Validator.build(payload.merge(method_name: __method__))
@@ -26,7 +37,26 @@ module Fcoin
         end
       end
 
-      # GET https://api.fcoin.com/v2/orders
+      # Get order list.
+      #
+      # curl: GET https://api.fcoin.com/v2/orders
+      #
+      # @note This method can not be invoked without authentication.
+      #
+      # @example get the canceled　order list of ethusdt limit 20 per page.
+      #  client = Fcoin::Client.new(api_key: ENV['FCOIN_API_KEY'], secret_key: ENV['FCOIN_SECRET_KEY'])
+      #  puts client.order_list(symbol: :ethusdt, states: :canceled, page_before: nil, page_after: nil, per_page: 20) #=> {"status":0,"data":[{"id":"L7rbALEIoI0ymo3uOXBF4gT4BlyTxgHhGoZjvptIv2U=","symbol":"ethusdt","amount":"0.001000000000000000","price":"1000.000000000000000000","created_at":1531714218130,"type":"limit","side":"sell","filled_amount":"0.000000000000000000","executed_value":"0.000000000000000000","fill_fees":"0.000000000000000000","source":"api","state":"canceled"},{"id":"32ZZCBEpPz2J9oFIJ4RMTIbypltjrVD9PAdYxQTHhUE=","symbol":"ethusdt","amount":"0.001000000000000000","price":"1000.000000000000000000","created_at":1531714092732,"type":"limit","side":"sell","filled_amount":"0.000000000000000000","executed_value":"0.000000000000000000","fill_fees":"0.000000000000000000","source":"api","state":"canceled"},{"id":"VotO2IKI2opgyKRd1lhR5bYj9zNZ398IW85gcBNPisU=","symbol":"ethusdt","amount":"0.001000000000000000","price":"1000.000000000000000000","created_at":1531712709955,"type":"limit","side":"sell","filled_amount":"0.000000000000000000","executed_value":"0.000000000000000000","fill_fees":"0.000000000000000000","source":"api","state":"canceled"},{"id":"tYH6LczJxaVe_WhsLOzOk4YM53hK2q169nYn9ReiwGM=","symbol":"ethusdt","amount":"0.001000000000000000","price":"1000.000000000000000000","created_at":1531675732267,"type":"limit","side":"sell","filled_amount":"0.000000000000000000","executed_value":"0.000000000000000000","fill_fees":"0.000000000000000000","source":"web","state":"canceled"},{"id":"U50WtZkmIh_bbuVKoipAMayCIy0A7qk4hBLxpDvKdPk=","symbol":"ethusdt","amount":"0.025800000000000000","price":"491.100000000000000000","created_at":1529665880201,"type":"limit","side":"buy","filled_amount":"0.000000000000000000","executed_value":"0.000000000000000000","fill_fees":"0.000000000000000000","source":"web","state":"canceled"}]}
+      #
+      #
+      # @see https://developer.fcoin.com/zh.html#9094989d10
+      # @raise [ArgumentError] If the symbol or states or per_page does not have.
+      # @raise [InvalidValueError] If symbol or states or per_page is invalid.
+      # @param symbol [String or Symbol] Transaction of pair.
+      # @param states [Stirng] Order states. states must be incldued in [submitted, partial_filled, canceled, partial_canceled, filled, pending_cancel]
+      # @param page_before [Integer] Query order before page number.
+      # @param page_after [Integer] Query order after page number.
+      # @param per_page [Integer] Order quantity per page. default is 20.
+      # @return [Hash] Returns order list.
       def order_list(symbol:, states:, page_before: nil, page_after: nil, per_page: 20)
         params = { symbol: symbol, states: states, before: page_before, after: page_after, limit: per_page }
         validator = Fcoin::Validator.build(params.merge(method_name: __method__))
@@ -38,18 +68,60 @@ module Fcoin
         end
       end
 
-      # GET https://api.fcoin.com/v2/orders/{order_id}
+      # Query order.
+      #
+      # curl: GET https://api.fcoin.com/v2/orders/$order_id
+      #
+      # @note This method can not be invoked without authentication.
+      #
+      # @example query order specified by order_id.
+      #  client = Fcoin::Client.new(api_key: ENV['FCOIN_API_KEY'], secret_key: ENV['FCOIN_SECRET_KEY'])
+      #  puts client.reference_order(order_id: "L7rbALEIoI0ymo3uOXBF4gT4Bl********jvptIv2U=") #=> {"status":0,"data":{"id":"L7rbALEIoI0ymo3uOXBF4gT4BlyTxgHhGoZjvptIv2U=","symbol":"ethusdt","amount":"0.001000000000000000","price":"1000.000000000000000000","created_at":1531714218130,"type":"limit","side":"sell","filled_amount":"0.000000000000000000","executed_value":"0.000000000000000000","fill_fees":"0.000000000000000000","source":"api","state":"canceled"}}
+      #
+      #
+      # @see https://developer.fcoin.com/zh.html#5faf03be01
+      # @raise [ArgumentError] If the order_id does not have.
+      # @param order_id [String]
+      # @return [Hash] Returns order specified by order_id.
       def reference_order(order_id:)
         get("orders/#{order_id}")
       end
       alias :order :reference_order
 
-      # POST https://api.fcoin.com/v2/orders/{order_id}/submit-cancel
+      # Cancel order.
+      #
+      # curl: POST https://api.fcoin.com/v2/orders/$order_id/submit-cancel
+      #
+      # @note This method can not be invoked without authentication.
+      #
+      # @example cancel order specified by order_id.
+      #  client = Fcoin::Client.new(api_key: ENV['FCOIN_API_KEY'], secret_key: ENV['FCOIN_SECRET_KEY'])
+      #  puts client.cancel_order(order_id: "nMEC_VrW0LYlP4iCcWzmdL50jFrvNWZoaQxvZSjeUSA=") #=> {"status":0}
+      #
+      #
+      # @see https://developer.fcoin.com/zh.html#299126e3bf
+      # @raise [ArgumentError] If the order_id does not have.
+      # @param order_id [String]
+      # @return [Hash]
       def cancel_order(order_id:)
         post("orders/#{order_id}/submit-cancel")
       end
 
-      # GET https://api.fcoin.com/v2/orders/{order_id}/match-results
+      # Query the transaction record for the specified by order_id.
+      #
+      # curl: GET https://api.fcoin.com/v2/orders/$order_id/match-results
+      #
+      # @note This method can not be invoked without authentication.
+      #
+      # @example Query the transaction record for the specified by order_id.
+      #  client = Fcoin::Client.new(api_key: ENV['FCOIN_API_KEY'], secret_key: ENV['FCOIN_SECRET_KEY'])
+      #  puts client.order_match_results(order_id: "kW3cRiXIGHG-cHNdter*********qfoMzbeHEQcqp4=") #=> {"status":0,"data":[{"price": "1000","fill_fees": "0.00010000000000000","filled_amount": "0.00500000000000000","side": "buy","type": "limit","created_at": 1531734385085}]}
+      #
+      #
+      # @see　https://developer.fcoin.com/zh.html#152112dfd6
+      # @raise [ArgumentError] If the order_id does not have.
+      # @param order_id [String]
+      # @return [Hash] Returns transaction record for the specified by order_id.
       def order_match_results(order_id:)
         get("orders/#{order_id}/match-results")
       end
