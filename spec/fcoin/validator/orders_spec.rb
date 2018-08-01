1ddcca65735e36aa8ae8f555cc3dbfd29558548c
yukihirop <te108186@gmail.com>
2018-08-01 22:57:29 +0900

    Rename && Modify Fcoin::OrdersValidator

--- spec/fcoin/validator/orders_spec.rb
+++ spec/fcoin/validator/orders_spec.rb
@@ -8,24 +8,59 @@ RSpec.describe Fcoin::OrdersValidator do
 
     context 'when create_order' do
       context 'when success' do
-        let(:params) { { symbol: :ethusdt, side: :sell, type: :limit, price: 1000, amount: 0.001, method_name: :create_order } }
-        it { is_expected.to eq  true }
+        context 'when limit' do
+          let(:params) { { symbol: :ethusdt, side: :sell, type: :limit, price: 1000, amount: 0.001, method_name: :create_order } }
+          it { is_expected.to eq true }
+
+          context 'when symbol do not exist' do
+            let(:params) { { symbol: :no_setting, side: :sell, type: :limit, price: 1000, amount: 0.001, method_name: :create_order } }
+            it { is_expected.to eq true }
+          end
+        end
+
+        context 'when market' do
+          let(:params) { { symbol: :ethusdt, side: :sell, type: :market, amount: 0.001, method_name: :create_order } }
+          it { is_expected.to eq true }
+
+          context 'when symbol do not exist' do
+            let(:params) { { symbol: :no_setting, side: :sell, type: :market, amount: 0.001, method_name: :create_order } }
+            it { is_expected.to eq true }
+          end
+        end
       end
 
       context 'when failure' do
-        let(:params) { { method_name: :create_order } }
-        it { is_expected.to eq false }
+        context 'when limit' do
+          let(:params) { { symbol: :ethusdt, side: :sell, type: :limit, price: 100000, amount: 0.00001, method_name: :create_order } }
+          it { is_expected.to eq false }
+
+          context 'when symbol is blank' do
+            let(:params) { { symbol: nil, side: :sell, type: :limit, price: 1000, amount: 0.001, method_name: :create_order } }
+            
+            it { is_expected.to eq false }
+          end
+        end
+
+        context 'when market' do
+          let(:params) { { symbol: :ethusdt, side: :sell, type: :market, amount: 0.00001, method_name: :create_order } }
+          it { is_expected.to eq false }
+
+          context 'when symbol is blank' do
+            let(:params) { { symbol: nil, side: :sell, type: :limit, price: 1000, amount: 0.001, method_name: :create_order } }
+            it { is_expected.to eq false }
+          end
+        end
       end
     end
 
     context 'when order_list' do
       context 'when success' do
-        let(:params) { { symbol: :ethusdt, states: :canceled, method_name: :order_list } }
+        let(:params) { { symbol: :ethusdt, states: 'canceled, filled', method_name: :order_list } }
         it { is_expected.to eq true }
       end
 
       context 'when failure' do
-        let(:params) { { method_name: :order_list } }
+        let(:params) { { symbol: nil, states: 'canceled, filled', method_name: :order_list } }
         it { is_expected.to eq false }
       end
     end
@@ -36,51 +71,75 @@ RSpec.describe Fcoin::OrdersValidator do
 
     context 'when create_order' do
       context 'when success' do
-        let(:params) { { symbol: :ethusdt, side: :sell, type: :limit, price: 1000, amount: 0.001, method_name: :create_order } }
-        it { is_expected.to be_blank }
+        context 'when limit' do
+          let(:params) { { symbol: :ethusdt, side: :sell, type: :limit, price: 1000, amount: 0.001, method_name: :create_order } }
+          it { is_expected.to be_blank }
+
+          context 'when symbol do not exist' do
+            let(:params) { { symbol: :no_setting, side: :sell, type: :limit, price: 1000, amount: 0.001, method_name: :create_order } }
+            it { is_expected.to be_blank }
+          end
+        end
+
+        context 'when market' do
+          let(:params) { { symbol: :ethusdt, side: :sell, type: :market, amount: 0.001, method_name: :create_order } }
+          it { is_expected.to be_blank }
+
+          context 'when symbol do not exist' do
+            let(:params) { { symbol: :no_setting, side: :sell, type: :market, amount: 0.001, method_name: :create_order } }
+            it { is_expected.to be_blank }
+          end
+        end
       end
 
       context 'when failure' do
-        context 'when symbol is nil' do
-          let(:params) { { method_name: :create_order } }
+        context 'when limit' do
+          let(:params) { { symbol: :ethusdt, side: :sell, type: :limit, price: 100000, amount: 0.00001, method_name: :create_order } }
           it { is_expected.to include({
-            :side => "side is nil. side is not included in the [buy, sell].",
-            :symbol => "symbol is nil. symbol can't be blank",
-            :type => "type is nil. type is not included in the [limit, market]."
-          }) }
+            :amount => "amount is 1.0e-05. amount is not between 0.001 and 10000.",
+            :price => "price is 100000. price is not between 1 and 10000.",
+            })
+          }
+
+          context 'when symbol is blank' do
+            let(:params) { { symbol: nil, side: :sell, type: :limit, price: 1000, amount: 0.001, method_name: :create_order } }
+            
+            it { is_expected.to include({
+              :symbol => "symbol is nil. symbol can't be blank",
+              }) }
+          end
         end
 
-        context 'when symbol is exist' do
-          let(:params) { { symbol: :ethusdt, method_name: :create_order } }
+        context 'when market' do
+          let(:params) { { symbol: :ethusdt, side: :sell, type: :market, amount: 0.00001, method_name: :create_order } }
           it { is_expected.to include({
-            :amount => "amount is nil. amount is not between 0.001 and 10000.",
-            :price => "price is nil. price is not between 1 and 10000.",
-            :side => "side is nil. side is not included in the [buy, sell].",
-            :type => "type is nil. type is not included in the [limit, market]."
-          }) }
+            :amount => "amount is 1.0e-05. amount is not between 0.001 and 10000.",
+            })
+          }
+
+          context 'when symbol is blank' do
+            let(:params) { { symbol: nil, side: :sell, type: :limit, price: 1000, amount: 0.001, method_name: :create_order } }
+            it { is_expected.to include({
+              :symbol => "symbol is nil. symbol can't be blank",
+              }) }
+          end
         end
       end
     end
 
     context 'when order_list' do
       context 'when success' do
-        let(:params) { { symbol: :ethusdt, states: :canceled, method_name: :order_list } }
+        let(:params) { { symbol: :ethusdt, states: 'canceled, filled', method_name: :order_list } }
         it { is_expected.to be_blank }
       end
 
       context 'when failure' do
-        context 'when symbol is nil' do
-          let(:params) { { method_name: :order_list } }
+        context 'when symbol is blank' do
+          let(:params) { { symbol: nil, side: :sell, type: :limit, price: 1000, amount: 0.001, method_name: :create_order } }
           it { is_expected.to include({
-            :symbol => "symbol is nil. symbol can't be blank"
-          }) }
-        end
-
-        context 'when symbol is exist' do
-          let(:params) { { symbol: :ethusdt, method_name: :order_list } }
-          it { is_expected.to include({
-            :states => "states is nil. states is not included in the [submitted, partial_filled, canceled, partial_canceled, filled, pending_cancel]."
-          }) }
+            :symbol => "symbol is nil. symbol can't be blank",
+            })
+          }
         end
       end
     end
