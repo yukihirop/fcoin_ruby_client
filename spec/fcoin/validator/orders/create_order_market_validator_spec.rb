34cd29220e3bd6273d65f311f1e3d6e242415ddb
yukihirop <te108186@gmail.com>
2018-08-04 02:58:08 +0900

    Modify Fcoin::Orders::CreateOrderMarketValidator

--- spec/fcoin/validator/orders/create_order_market_validator_spec.rb
+++ spec/fcoin/validator/orders/create_order_market_validator_spec.rb
@@ -7,27 +7,56 @@ RSpec.describe Fcoin::Orders::CreateOrderMarketValidator do
     subject { validator.valid? }
 
     context 'when success' do
-      let(:params) { { symbol: :ethusdt, side: :sell, amount: 0.001 } }
-      it { is_expected.to eq true }
-
-      context 'when setting do not exist' do
-        let(:params) { { symbol: :not_setting, side: :sell, amount: 0.001 } }
+      context 'when sell' do
+        let(:params) { { symbol: :ethusdt, side: :sell, amount: 0.001 } }
         it { is_expected.to eq true }
+
+        context 'when setting do not exist' do
+          let(:params) { { symbol: :not_setting, side: :sell, amount: 0.001 } }
+          it { is_expected.to eq true }
+        end
+      end
+
+      context 'when buy' do
+        let(:params) { { symbol: :ethusdt, side: :buy, amount: 1 } }
+        it { is_expected.to eq true }
+
+        context 'when setting do not exist' do
+          let(:params) { { symbol: :not_setting, side: :buy, amount: 1} }
+          it { is_expected.to eq true }
+        end
       end
     end
 
     context 'when failure' do
-      let(:params) { { symbol: :ethusdt, side: :sell, amount: 0.0001 } }
-      it { is_expected.to eq false }
-
-      context 'when symbol do not exist' do
-        let(:params) { { symbol: nil, side: :sell, amount: 0.0001 } }
+      context 'when sell' do
+        let(:params) { { symbol: :ethusdt, side: :sell, amount: 0.0001 } }
         it { is_expected.to eq false }
+
+        context 'when symbol do not exist' do
+          let(:params) { { symbol: nil, side: :sell, amount: 0.0001 } }
+          it { is_expected.to eq false }
+        end
+
+        context 'when do not adapted on-going order' do
+          let(:params) { { symbol: :fiusdt, side: :sell, amount: 0.0001 } }
+          it { is_expected.to eq false }
+        end
       end
-
-      context 'when do not adapted on-going order' do
-        let(:params) { { symbol: :fiusdt, side: :sell, amount: 0.0001 } }
+      
+      context 'when buy' do
+        let(:params) { { symbol: :ethusdt, side: :buy, amount: 0.0001 } }
         it { is_expected.to eq false }
+
+        context 'when symbol do not exist' do
+          let(:params) { { symbol: nil, side: :buy, amount: 0.0001 } }
+          it { is_expected.to eq false }
+        end
+
+        context 'when do not adapted on-going order' do
+          let(:params) { { symbol: :fiusdt, side: :buy, amount: 0.0001 } }
+          it { is_expected.to eq false }
+        end
       end
     end
   end
@@ -36,36 +65,71 @@ RSpec.describe Fcoin::Orders::CreateOrderMarketValidator do
     subject { validator.messages }
 
     context 'when valid' do
-      let(:params) { { symbol: :ethusdt, side: :sell, price: 1000, amount: 0.001 } }
-      it { is_expected.to be_blank }
-
-      context 'when setting do not exist' do
-        let(:params) { { symbol: :not_setting, side: :sell, price: 1000, amount: 0.001 } }
+      context 'when sell' do
+        let(:params) { { symbol: :ethusdt, side: :sell, amount: 0.001 } }
         it { is_expected.to be_blank }
+
+        context 'when setting do not exist' do
+          let(:params) { { symbol: :not_setting, side: :sell, amount: 0.001 } }
+          it { is_expected.to be_blank }
+        end
+      end
+      
+      context 'when buy' do
+        let(:params) { { symbol: :ethusdt, side: :buy, amount: 1 } }
+        it { is_expected.to be_blank }
+
+        context 'when setting do not exist' do
+          let(:params) { { symbol: :not_setting, side: :buy, amount: 1} }
+          it { is_expected.to be_blank }
+        end
       end
     end
 
     context 'when invalid' do
-      let(:params) { { symbol: :ethusdt, side: :sell, price: 100000, amount: 0.0001 } }
-      it { is_expected.to include({
-        :amount => "amount is 0.0001. amount is not between 0.001 and 10000.",
-        })
-      }
-
-      context 'when symbol do not exist' do
-        let(:params) { { symbol: nil, side: :sell, price: 100000, amount: 0.0001 } }
+      context 'when sell' do
+        let(:params) { { symbol: :ethusdt, side: :sell, amount: 0.0001 } }
         it { is_expected.to include({
-          :symbol => "symbol is nil. symbol can't be blank",
+          :amount => "amount is 0.0001. amount is not between 0.001 and 10000.",
           })
         }
+
+        context 'when symbol do not exist' do
+          let(:params) { { symbol: nil, side: :sell, amount: 0.0001 } }
+          it { is_expected.to include({
+            :symbol => "symbol is nil. symbol can't be blank",
+            })
+          }
+        end
+
+        context 'when do not adapted on-going order' do
+          let(:params) { { symbol: :fiusdt, side: :sell, amount: 0.0001 } }
+          it { is_expected.to include({
+            :symbol => "symbol is fiusdt. This symbol board is not adapted on-going order.",
+            })
+          }
+        end
       end
 
-      context 'when do not adapted on-going order' do
-        let(:params) { { symbol: :fiusdt, side: :sell, amount: 0.0001 } }
+      context 'when buy' do
+        let(:params) { { symbol: :ethusdt, side: :buy, amount: 0.00001 } }
         it { is_expected.to include({
-          :symbol => "symbol is fiusdt. This symbol board is not adapted on-going order.",
-          })
-        }
+          :total => "total is 1.0e-05. total is not between 0.001 and 10000.",
+          })}
+
+        context 'when symbol do not exist' do
+          let(:params) { { symbol: :fiusdt, side: :buy, amount: 0.00001 } }
+          it { is_expected.to include({
+            :symbol => "symbol is fiusdt. This symbol board is not adapted on-going order.",
+            })}
+        end
+
+        context 'when do not adapted on-going order' do
+          let(:params) { { symbol: :fiusdt, side: :buy, amount: 0.00001 } }
+          it { is_expected.to include({
+            :symbol => "symbol is fiusdt. This symbol board is not adapted on-going order.",
+            })}
+        end
       end
     end
   end
