a78080a00a308ffa735cf0ce00349b4a781d3ce5
yukihirop <te108186@gmail.com>
2018-08-04 15:30:43 +0900

    Add simplecov settings

---
+++ spec/support/simplecov.rb
@@ -0,0 +1,12 @@
+require 'simplecov'
+require 'coveralls'
+Coveralls.wear!
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+
+SimpleCov.start do
+  add_filter %w(.bundle bin doc examples log spec/support)
+end
